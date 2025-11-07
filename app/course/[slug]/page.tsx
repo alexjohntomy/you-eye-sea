005f7c9ea7a121577bc7b471fbc6706ab39579be
alexjohntomy <111886094+alexjohntomy@users.noreply.github.com>
2025-11-07 11:47:29 -0600

    Added hyperlinks to course catalog
    - Suggested

--- app/course/[slug]/page.tsx
+++ app/course/[slug]/page.tsx
@@ -4,7 +4,9 @@ import { Badge } from "@/components/ui/badge"
 import { CommentsPaneServer } from '@/components/custom/comments-pane-server'
 import { TablePaneServer } from '@/components/custom/table-pane-server'
 import { formatGradeData } from '@/app/_util/formatGradeData'
+import Link from "next/link"
 import type { Metadata } from 'next'
+import { ExternalLink } from 'lucide-react';
 
 // NEED TO TURN THESE INTO FUNCTIONAL COMPONENTS LATER
 import {
@@ -68,22 +70,29 @@ export default async function CourseDetailsPage({
   const { slug } = await params
   const courseDetails = await getCourseDetails(slug)
   const GradeDistributionCount = await getCourseInstance(slug)
+  const encodedURL = "https://catalog.uic.edu/ucat/course-descriptions/cs/#:~:text=" + encodeURIComponent(`${courseDetails.name} ${courseDetails.number}`)
+
   
   return (
     <div className="flex flex-col md:flex-row grow bg-background w-full h-full md:h-[calc(100svh-120px)] overflow-hidden">
-
       {/* Stats */}
       <div className="flex flex-col w-full md:w-1/2 p-8 h-full gap-2">
           <div className='flex-row'>
-            <h1 className="text-uic-red-600 font-black text-4xl">{courseDetails.name} {courseDetails.number}</h1>
-            <p className="text-foreground/70 font-medium text-lg">{courseDetails.title}</p>
+                <h1 className="text-uic-red-600 font-black text-4xl">{courseDetails.name} {courseDetails.number}</h1>
+                <p className="text-foreground/70 font-medium text-lg">{courseDetails.title}</p>
           </div>
+          <Link href = {encodedURL}>
+              <Badge variant="outline" className="flex-row gap-2 relative bottom-1 rounded-md text-xs font-semibold px-3 py-2 bg-badge-bg/90 text-badge-text border-badge-border/20 shadow-[inset_0px_-6px_10px_2px_var(--badge-shadow-base)]/40 hover:bg-badge-bg ">
+                View in Course Catalog
+                <ExternalLink className='opacity-70 relative'/>
+              </Badge>
+          </Link>
             <div className="">
                 <GradeDistributionChart chartData={GradeDistributionCount}></GradeDistributionChart>
             </div>
             <h5 className='py-2 text-xs text-center text-foreground/50'>Data is sourced from official UIC grade distributions but statistics is not my strong suit so could be wrong.</h5>
       </div>
-      
+
       {/* Comments */}
         <section className='w-full md:max-w-1/4 md:w-1/4 h-full relative border-r border-l border-foreground/10'>
             <CommentsPaneServer slug={slug}/>
