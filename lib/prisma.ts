e60627825b632b9d804bbf2d826d16ece600dc19
alexjohntomy <111886094+alexjohntomy@users.noreply.github.com>
2026-03-13 23:37:37 -0500

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,20 @@
-import { PrismaClient } from "../src/generated/prisma/client";
+// import { PrismaClient } from "../src/generated/prisma/client";
+// import { withAccelerate } from "@prisma/extension-accelerate";
+
+// const globalForPrisma = global as unknown as {
+//   prisma: PrismaClient;
+// };
+
+// const prisma =
+//   globalForPrisma.prisma || new PrismaClient({accelerateUrl: process.env.DATABASE_URL!}).$extends(withAccelerate());
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// export default prisma;
+
+import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
-
-const prisma =
-  globalForPrisma.prisma || new PrismaClient({accelerateUrl: process.env.DATABASE_URL!}).$extends(withAccelerate());
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export const prisma = new PrismaClient().$extends(withAccelerate());
 
 export default prisma;
