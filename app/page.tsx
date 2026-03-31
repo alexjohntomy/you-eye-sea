65181572a412f107f9d1689d0648e4e44d577f59
alexjohntomy <111886094+alexjohntomy@users.noreply.github.com>
2026-03-30 23:56:16 -0500

    Minor fix to prevent scroll in Safari

--- app/page.tsx
+++ app/page.tsx
@@ -13,16 +13,16 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="animate-in fade-in flex min-h-[calc(100svh-4rem)] w-full flex-col px-1 py-1 duration-700 md:min-h-full md:justify-center">
+    <div className="animate-in fade-in flex min-h-svh max-h-svh w-full flex-col overflow-hidden px-1 py-1 duration-700 md:justify-center">
       {/* Hero Text */}
-      <div className="mt-4.5 mb-3 flex flex-col items-center md:mt-10 md:mb-6 md:gap-2">
+      <div className="mt-6 mb-3 flex flex-col items-center md:mt-10 md:mb-6 md:gap-2">
         <Badge
           variant="outline"
           className="bg-background/70 border-uic-navy-700/20 dark:border-secondary/20 text-uic-navy-700/70 dark:text-uic-navy-700/50 relative bottom-2 flex px-2 py-1 md:inline-flex"
         >
           Fall 2026 Registration Now Open
         </Badge>
-        <h1 className="bg-text-gradient-uic bg-clip-text px-5 pt-0 pb-0 text-center text-2xl font-black text-transparent text-shadow-[0px_0px_0px_20px_var(--inset-color)] md:text-5xl">
+        <h1 className="bg-text-gradient-uic bg-clip-text px-5 pt-0 pb-0 text-center text-3xl font-black text-transparent text-shadow-[0px_0px_0px_20px_var(--inset-color)] md:text-5xl">
           Find Your Next Class<span className="hidden md:inline"> Today</span>
         </h1>
         <h2 className="text-l text-uic-navy-800 px-10 text-center font-semibold opacity-80 md:text-xl">
